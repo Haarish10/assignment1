827fb19add1ba033b8909ad8b23b41b35f130fe5
Haarish10 <120053507+Haarish10@users.noreply.github.com>
2024-05-01 23:21:56 +0530

    Update assingment1.sql

--- assingment1.sql
+++ assingment1.sql
@@ -4,13 +4,13 @@ use ticketbookingsystem;
 
 create table venue (
     venue_id int primary key,
-    venue_name varchar(255) not null,
-    address varchar(255) not null
+    venue_name varchar(20) not null,
+    address varchar(20) not null
 );
 
 create table event (
     event_id int primary key,
-    event_name varchar(255) not null,
+    event_name varchar(20) not null,
     event_date date,
     event_time time,
     venue_id int,
@@ -24,8 +24,8 @@ create table event (
 
 create table customer (
     customer_id int primary key,
-    customer_name varchar(255) not null,
-    email varchar(255),
+    customer_name varchar(20) not null,
+    email varchar(20),
     phone_number varchar(15),
     booking_id int,
     foreign key (booking_id) references booking(booking_id) 
@@ -37,21 +37,21 @@ create table booking (
     event_id int,
     num_tickets int,
     total_cost decimal(10, 2),
-    booking_date datetime default current_timestamp,
+    booking_date date,
     foreign key (customer_id) references customer(customer_id),
 );
 
-insert into venu (venue_id, venue_name, address) values
+insert into venue (venue_id, venue_name, address) values
 (1, 'venue 1', 'address 1'),
 (2, 'venue 2', 'address 2'),
 (3, 'venue 3', 'address 3'),
 (4, 'venue 4', 'address 4'),
 (5, 'venue 5', 'address 5');
-(6, 'venue 6', 'address 5');
-(7, 'venue 7', 'address 5');
-(8, 'venue 8', 'address 5');
-(9, 'venue 9', 'address 5');
-(10, 'venue 10', 'address 5');
+(6, 'venue 6', 'address 6');
+(7, 'venue 7', 'address 7');
+(8, 'venue 8', 'address 8');
+(9, 'venue 9', 'address 9');
+(10, 'venue 10', 'address 10');
 
 insert into event (event_id, event_name, event_date, event_time, venue_id, total_seats, available_seats, ticket_price, event_type, booking_id)
 values
@@ -60,11 +60,11 @@ values
 (3, 'event 3', '2024-05-03', '20:00:00', 3, 300, 200, 1800.00, 'concert', 103),
 (4, 'event 4', '2024-05-04', '21:00:00', 4, 250, 100, 2200.00, 'movie', 104),
 (5, 'event 5', '2024-05-05', '22:00:00', 5, 400, 300, 2500.00, 'concert',  105);
-(6, 'event 6', '2024-05-05', '22:00:00', 5, 400, 300, 2500.00, 'concert', 106);
-(7, 'event 7', '2024-05-05', '22:00:00', 5, 400, 300, 2500.00, 'concert', 107);
-(8, 'event 8', '2024-05-05', '22:00:00', 5, 400, 300, 2500.00, 'concert', 108);
-(9, 'event 9', '2024-05-05', '22:00:00', 5, 400, 300, 2500.00, 'concert', 109);
-(10, 'event 10', '2024-05-05', '22:00:00', 5, 400, 300, 2500.00, 'concert', 110);
+(6, 'event 6', '2024-05-06', '22:00:00', 5, 400, 300, 2500.00, 'concert', 106);
+(7, 'event 7', '2024-05-07', '22:00:00', 5, 400, 300, 2500.00, 'concert', 107);
+(8, 'event 8', '2024-05-08', '22:00:00', 5, 400, 300, 2500.00, 'concert', 108);
+(9, 'event 9', '2024-05-09', '22:00:00', 5, 400, 300, 2500.00, 'concert', 109);
+(10, 'event 10', '2024-05-10', '22:00:00', 5, 400, 300, 2500.00, 'concert', 110);
 
 insert into customer (customer_id, customer_name, email, phone_number, booking_id)
 values
@@ -116,21 +116,18 @@ select * from event where total_seats > 15000 order by event_name;
 
 select * from event where event_name not like 'x%' and event_name not like 'y%' and event_name not like 'z%';
 
-select event_id, event_name, avg(ticket_price) as avg_ticket_price
-from event
+
+select event_id, event_name, avg(ticket_price) from event
 group by event_id, event_name;
 
-select sum(total_cost) as total_revenue
-from booking;
+select sum(total_cost) from booking;
 
-select event_id, event_name, sum(num_tickets) as total_tickets_sold
-from booking
+select event_id, event_name, sum(num_tickets) from booking
 group by event_id, event_name
 order by total_tickets_sold desc
 limit 1;
 
-select event_id, event_name, sum(num_tickets) as total_tickets_sold
-from booking`
+select event_id, event_name, sum(num_tickets) from booking`
 group by event_id, event_name;
 
 select event_id, event_name
@@ -138,124 +135,80 @@ from event
 left join booking on event.event_id = booking.event_id
 where booking.booking_id is null;
 
-select customer_id, customer_name, count(booking_id) as total_bookings
+select customer_id, customer_name, count(booking_id) 
 from booking
 group by customer_id, customer_name
 order by total_bookings desc
 limit 1;
 
-select month(event_date) as month, sum(num_tickets) as total_tickets_sold
+select month(event_date) as month, sum(num_tickets) 
 from booking
 join event on booking.event_id = event.event_id
 group by month(event_date);
 
-select venue_id, venue_name, avg(ticket_price) as avg_ticket_price
-from event
+select venue_id, venue_name, avg(ticket_price) from event
 join venu on event.venue_id = venu.venue_id
 group by venue_id, venue_name;
 
-select event_type, sum(num_tickets) as total_tickets_sold
-from event
+select event_type, sum(num_tickets) from event
 join booking on event.event_id = booking.event_id
 group by event_type;
 
-select year(event_date) as year, sum(total_cost) as total_revenue
+select year(event_date) as year, sum(total_cost) 
 from booking
 join event on booking.event_id = event.event_id
 group by year(event_date);
 
-select customer_id, customer_name, count(distinct event_id) as total_events_booked
+select customer_id, customer_name, count(distinct event_id) 
 from booking
 join customer on booking.customer_id = customer.customer_id
 group by customer_id, customer_name
 having total_events_booked > 1;
 
-select customer_id, customer_name, sum(total_cost) as total_revenue
+select customer_id, customer_name, sum(total_cost) 
 from booking
 join customer on booking.customer_id = customer.customer_id
 group by customer_id, customer_name;
 
-select venu.venue_id, venu.venue_name, event.event_type, avg(event.ticket_price) as avg_ticket_price
+select venu.venue_id, venu.venue_name, event.event_type, avg(event.ticket_price) 
 from event
 join venu on event.venue_id = venu.venue_id
 group by venu.venue_id, venu.venue_name, event.event_type;
 
-select customer_id, customer_name, count(booking_id) as total_tickets_purchased
+select customer_id, customer_name, count(booking_id) 
 from booking
 join customer on booking.customer_id = customer.customer_id
 where booking_date >= date_sub(curdate(), interval 30 day)
 group by customer_id, customer_name;
 
-select venue_id, venue_name,
-       (select avg(ticket_price) 
-        from event 
-        where event.venue_id = venu.venue_id) as avg_ticket_price
-from venu;
+select venue_name,ticket_price from venue v, event e 
+where ticket_price=(select avg(ticket_price) from event e where e.venue_id = v.venue_id);
 
-select event_id, event_name
-from event
-where (select sum(num_tickets) from booking where booking.event_id = event.event_id) > 0.5 * total_seats;
+select event_name,(total_seats-available_seats) from event 
+where (select(total_seats-available_seats) from event e where e.event_id=event.event_id)>0.5*total_seats;
 
-select event.event_id, event.event_name, (sum(booking.num_tickets), 0) as total_tickets_sold
-from event
-left join booking on event.event_id = booking.event_id
-group by event.event_id, event.event_name;
+select event_name,(select sum(total_seats - available_seats) from event e 
+where e.event_id = event.event_id) as total_tickets_sold from event;
 
-select customer_id, customer_name
-from customer
-where not exists (
-    select * from booking where booking.customer_id = customer.customer_id
-);
+select c.customer_name from customer c where not exists (select booking_id from booking where customer_id = c.customer_id);
 
-select event_id, event_name
-from event
-where event_id not in (select distinct event_id from booking);
+select  event_id, event_name from event where (total_seats - available_seats) not in (select (total_seats - available_seats) from event where (total_seats - available_seats) <>0);
 
-select event_type, sum(total_tickets_sold) as total_tickets_sold
-from (
-    select event.event_type, (sum(booking.num_tickets), 0) as total_tickets_sold
-    from event
-    left join booking on event.event_id = booking.event_id
-    group by event.event_id, event.event_type
-) as subquery
-group by event_type;
+select e.event_type, sum(tickets_sold) as tickets_sold from(select event_id, sum(num_tickets) as tickets_sold from booking group by event_id) b join event e on b.event_id = e.event_id group by e.event_type;
 
-select event_id, event_name, ticket_price
-from event
+select event_name, ticket_price from event 
 where ticket_price > (select avg(ticket_price) from event);
 
-select customer.customer_id, customer.customer_name,
-       (select (sum(booking.total_cost), 0) 
-        from booking 
-        where booking.customer_id = customer.customer_id) as total_revenue
-from customer;
+select c.customer_id, c.customer_name, (select SUM(b.total_cost) from Booking b 
+where b.customer_id = c.customer_id) as total_revenue from customer c;
 
-select customer_id, customer_name
-from customer
-where exists (
-    select * from booking
-    join event on booking.event_id = event.event_id
-    where event.venue_id = (select venue_id from venu where venue_name = 'given venue name')
-      and booking.customer_id = customer.customer_id
-);
+select customer_id, customer_name from customer where exists (select 1 from booking join event on booking.event_id = event.event_id where venue_id = 3 and booking.customer_id = customer.customer_id);
 
-select event_type, (sum(total_tickets_sold), 0) as total_tickets_sold
-from (
-    select event.event_type, (sum(booking.num_tickets), 0) as total_tickets_sold
-    from event
-    left join booking on event.event_id = booking.event_id
-    group by event.event_id, event.event_type
-) as subquery
-group by event_type;
+select e.event_type, sum(b.num_tickets) from event e join booking b on b.event_id = e.event_id 
+where b.num_tickets = (select sum(b.num_tickets) from booking B where B.event_id = e.event_id)group by e.event_type;
 
-select customer_id, customer_name, month(booking_date) as booking_month
-from booking
-join customer on booking.customer_id = customer.customer_id
-group by customer_id, customer_name, booking_month;
+select customer_name, month(booking_date) from customer c join booking b on b.customer_id=c.customer_id 
+where month(booking_date) = (select month(booking_date) from booking b limit 1);
 
-select venue_id, venue_name,
-       (select avg(ticket_price) 
-        from event 
-        where event.venue_id = venu.venue_id) as avg_ticket_price
-from venu;
-
+select venue_name,(select avg(ticket_price)from event
+where venue_id = v.venue_id) as avg_price from Venue v order by avg_price;
